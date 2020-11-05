aba9c833b347907b0fcd7c2759fce3bbd00e8b87
MeerimMoidin <ramissara18@gmail.com>
2020-11-05 15:24:57 -0600

    MeerimPage

---
+++ src/test/java/com/Librarian/pages/MeerimPAge.java
@@ -0,0 +1,38 @@
+package com.Librarian.pages;
+
+import com.Librarian.utils.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class MeerimPAge extends BasePage{
+    @FindBy(id="book_categories")
+    private WebElement bookCategoriesDropDown;
+
+    @FindBy(xpath ="//table[@id='tbl_books']//tr/td[5]")
+    private List<WebElement> displayCategories;
+
+    public void bookCategories(String categori){
+
+        BrowserUtils.wait(1);
+        Select select=new Select(bookCategoriesDropDown);
+        select.selectByVisibleText(categori);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
