2ff9cc2c46a641e03df657436026710c2cfd8dc0
Alex-B-I <alex.berloh@gmail.com>
2020-11-09 16:34:03 -0600

    added plugin

--- src/test/java/com/Librarian/runners/CucumberRunner.java
+++ src/test/java/com/Librarian/runners/CucumberRunner.java
@@ -6,6 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json"        //needed for cucumber report
+        },
         features = "src/test/resources/features",
         glue = "com/Librarian/step_definitions",
         dryRun = false,
